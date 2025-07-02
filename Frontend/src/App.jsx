f9d1d21510394022f4c3e693dc26d512ae647421
SPARSH GUPTA <sparshgupta2109@gmail.com>
2025-07-02 18:08:37 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
 import Products from './components/Products'
@@ -13,7 +13,7 @@ const App = () => {
 
   return (
     <div className='flex flex-col'>
-      <Router>
+      <BrowserRouter>
           <Routes>
             <Route path='/' element={token ? <Products/> : <Navigate to='/login'/>}/>
             <Route path='/login' element={<Login/>}/>
@@ -21,7 +21,7 @@ const App = () => {
             <Route path='/feedbacks' element={token ? <Feedbacks/> : <Navigate to='/login'/>}/>
             <Route path='/profile' element={token ? <Profile/> : <Navigate to='/login'/>}/>
           </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   )
 }
