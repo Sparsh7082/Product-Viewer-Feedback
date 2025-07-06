6e4dc01c9ddfceaf0a635d24cc6e6d5b3ca21aa3
SPARSH GUPTA <sparshgupta2109@gmail.com>
2025-07-06 17:48:59 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const res = await axios.post('http://localhost:8080/api/auth/login',{
+            const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/login`,{
                 email,
                 password
             })
