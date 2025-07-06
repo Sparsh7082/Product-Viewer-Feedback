2762e5c9dc097934fa572a40e9e61fde1973379f
SPARSH GUPTA <sparshgupta2109@gmail.com>
2025-07-06 18:21:15 +0530

    remove cors content

--- Backend/server.js
+++ Backend/server.js
@@ -11,10 +11,7 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}))
+app.use(cors())
 
 app.use('/api/auth',authRoutes)
 app.use('/api/feedback',feedbackRoutes)
